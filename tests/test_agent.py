23ea436aa2bcecb37bb42b473b152570ea988acc
victor cereceda <victorcerecedagonzalez@gmail.com>
2026-09-05 14:28:49 +0200

    test for every services except ollama and nginx complete. TEST COMPLETE

--- tests/test_agent.py
+++ tests/test_agent.py
@@ -27,3 +27,33 @@ async def test_to_agent_service():
                 assert data["message"] == "test mensaje ok"
                 print("TEST MENSAJE OK !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
                 break
+
+
+@pytest.mark.asyncio
+async def test_agent_executes_calculator_tool():
+    async with websockets.connect(AGENT_WS_URL) as websocket:
+
+        message = {
+            "type": "message",
+            "conversation_id": None,
+            "message": "2+2",
+        }
+
+        await websocket.send(json.dumps(message))
+
+        tool_execution_started = False
+
+        while True:
+            response = await websocket.recv()
+            data = json.loads(response)
+
+            if (
+                data["type"] == "status"
+                and data["message"] == "ejecutando herramienta del agente"
+            ):
+                tool_execution_started = True
+
+            if data["type"] == "finished":
+                assert tool_execution_started is True
+                assert data["message"] == "4"
+                break
